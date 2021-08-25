8fd93d617567ed6b2414deb307e704fab8effd9f
Kim, Jungwon <kimj@ornl.gov>
2021-08-24 20:25:19 -0500

    Added CTestConfig.cmake

---
+++ CTestConfig.cmake
@@ -0,0 +1,8 @@
+set(CTEST_PROJECT_NAME "IRIS")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
+
+set(CTEST_DROP_METHOD "https")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=IRIS")
+set(CTEST_DROP_SITE_CDASH TRUE)
+

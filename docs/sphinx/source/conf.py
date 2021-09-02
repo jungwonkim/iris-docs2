2afdd1b3ccd60603455a23d4088495799a050dfa
Kim, Jungwon <kimj@ornl.gov>
2021-09-02 01:20:18 -0500

    docs.yml

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -36,9 +36,9 @@ extensions = [
     "breathe"
 ]
 
-subprocess.call('cd ../../doxygen/; doxygen', shell=True)
+subprocess.call('cd ../../doxygen/; doxygen Doxyfile.in', shell=True)
 
-breathe_projects = {"C": "../../doxygen/xml/"}
+breathe_projects = {"C": "../../../docs/doxygen/build/xml/"}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -56,8 +56,26 @@ exclude_patterns = []
 #
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'analytics_id': '',
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': '',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+github_url = 'https://github.com/jungwonkim/iris'
--- docs/sphinx/source/misc.rst
+++ docs/sphinx/source/misc.rst
@@ -4,7 +4,7 @@ Citation
 ==================
 To cite IRIS, please use the following paper.
 
-Jungwon Kim, Seyong Lee, Beau Johnston, and Jeffrey S. Vetter. 2021. IRIS: A Portable Runtime System Exploiting Multiple Heterogeneous Programming Systems. In *Proceedings of the 25th IEEE High Performance Extreme Computing Conference (HPEC '21)*.
+Jungwon Kim, Seyong Lee, Beau Johnston, and Jeffrey S. Vetter. 2021. IRIS: A Portable Runtime System Exploiting Multiple Heterogeneous Programming Systems. In *Proceedings of the 25th IEEE High Performance Extreme Computing Conference (HPEC '21)*. 1--8.
 
 .. code-block:: bibtex
 
